61b1e2a5bd1d0b4db788c271ef7cff6539a7e2f5
Carolin Anto <carolinanto906@gmail.com>
2026-03-22 02:53:02 +0530

    new autoencoder

--- models/train.py
+++ models/train.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Model
@@ -23,38 +26,77 @@ print("Scaling data...")
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X_raw)
 
-
 # -----------------------------
-# 3. BUILD AUTOENCODER
+# VAE PARAMETERS
 # -----------------------------
-print("Building model...")
-
 input_dim = X.shape[1]
+latent_dim = 2
 
-input_layer = Input(shape=(input_dim,))
-encoded = Dense(6, activation='relu')(input_layer)
-encoded = Dense(3, activation='relu')(encoded)
+# -----------------------------
+# ENCODER
+# -----------------------------
+inputs = tf.keras.Input(shape=(input_dim,))
+h = layers.Dense(8, activation='relu')(inputs)
+h = layers.Dense(4, activation='relu')(h)
 
-decoded = Dense(6, activation='relu')(encoded)
-decoded = Dense(input_dim, activation='sigmoid')(decoded)
+z_mean = layers.Dense(latent_dim)(h)
+z_log_var = layers.Dense(latent_dim)(h)
 
-autoencoder = Model(inputs=input_layer, outputs=decoded)
-autoencoder.compile(optimizer='adam', loss='mse')
+
+# Sampling function
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = tf.random.normal(shape=(tf.shape(z_mean)[0], latent_dim))
+    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+z = layers.Lambda(sampling)([z_mean, z_log_var])
 
 
 # -----------------------------
-# 4. TRAIN MODEL
+# DECODER
 # -----------------------------
-print("Training model...")
-autoencoder.fit(X, X, epochs=30, batch_size=8, verbose=1)
+decoder_h = layers.Dense(4, activation='relu')
+decoder_h2 = layers.Dense(8, activation='relu')
+decoder_out = layers.Dense(input_dim, activation='sigmoid')
+
+h_decoded = decoder_h(z)
+h_decoded = decoder_h2(h_decoded)
+outputs = decoder_out(h_decoded)
 
 
+# -----------------------------
+# VAE MODEL
+# -----------------------------
+vae = tf.keras.Model(inputs, outputs)
+
+
+# -----------------------------
+# LOSS FUNCTION (FIXED)
+# -----------------------------
+reconstruction_loss = tf.keras.losses.mse(inputs, outputs)
+reconstruction_loss = tf.reduce_mean(reconstruction_loss) * input_dim
+
+kl_loss = -0.5 * tf.reduce_mean(
+    1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+)
+
+vae_loss = reconstruction_loss + kl_loss
+
+vae.add_loss(vae_loss)
+vae.compile(optimizer='adam')
+
+
+# -----------------------------
+# TRAIN
+# -----------------------------
+vae.fit(X, X, epochs=30, batch_size=8, verbose=1)
 # -----------------------------
 # 5. ANOMALY DETECTION
 # -----------------------------
 print("Calculating anomaly scores...")
 
-reconstructed = autoencoder.predict(X)
+reconstructed = vae.predict(X)
 error = np.mean((X - reconstructed) ** 2, axis=1)
 
 df['anomaly_score'] = error
